27fc82e52ad3d2caf82e4df24cbe106f966604c2
jkr2255 <jkr2255@users.noreply.github.com>
2018-01-01 17:37:51 +0900

    remove export (little use for exported value)

--- src/entry.js
+++ src/entry.js
@@ -4,7 +4,6 @@ import fontawesome from '@fortawesome/fontawesome';
 
 fontawesome.noAuto();
 
-import iconTag from './tags/font-awesome-icon';
+import './tags/font-awesome-icon';
 import './tags/font-awesome-layers.tag'
 
-export default iconTag;
