bf20e295d2d6881d8e77b7efd1b244abc5347735
jkr2255 <jkr2255@users.noreply.github.com>
2018-01-28 19:15:44 +0900

    Packager changed from Webpack to Rollup

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import riot from 'rollup-plugin-riot';
+import uglify from 'rollup-plugin-uglify';
+
+export default {
+  input: 'src/entry.js',
+  plugins: [
+    riot(),
+    nodeResolve({ jsnext: true }),
+    commonjs(),
+    babel(),
+    uglify()
+  ],
+  output: {
+    file: 'index.js',
+    format: 'umd',
+    name: 'RiotFontAwesome',
+    globals: {
+      riot: 'riot',
+      '@fortawesome/fontawesome': 'FontAwesome'
+    }
+  },
+  external: [
+    'riot',
+    '@fortawesome/fontawesome'
+  ]
+};
